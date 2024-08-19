1abb881079fd2211504403d0e5a00d28b563efbd
KalliopiK26 <144463538+KalliopiK26@users.noreply.github.com>
2024-08-19 10:55:38 -0400

    Update Publisher _member_function.py

--- Publisher _member_function.py
+++ Publisher _member_function.py
@@ -1,13 +1,13 @@
 import rclpy
 import random
+import time
 from rclpy.node import Node
-from std_msgs.msg import String
 from geometry_msgs.msg import Point
 
 class MinimalPublisher(Node):
     def __init__(self):
         super().__init__('minimal_publisher')
-        self.publisher_ = self.create_publisher(String, 'topic', 10)
+        self.publisher_ = self.create_publisher(Point, 'topic', 10)
         timer_period = 1  # seconds
         self.timer = self.create_timer(timer_period, self.timer_callback)
         self.i = 0
@@ -18,14 +18,11 @@ class MinimalPublisher(Node):
         msg.x = random.uniform(0,100)
         msg.y = random.uniform(0,100)
         msg.z = random.uniform(0,100)
-        
-        output = String()
-        output.data = f'x={msg.x}, y={msg.y}, z={msg.z}'
-        
-        self.publisher_.publish(output)
-        self.get_logger().info('Publishing: "%s"' % output.data)
-        self.i += 1
 
+        self.publisher_.publish(msg)
+        self.get_logger().info(f'Publishing: x={msg.x}, y={msg.y}, z={msg.z}')
+        self.i += 1
+        time.sleep(3)
 def main(args=None):
     rclpy.init(args=args)
     minimal_publisher = MinimalPublisher()
